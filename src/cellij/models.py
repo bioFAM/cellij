93ed260c6c31c60d0a7d737d5a20719b93cfd8ca
martin.rohbeck <martin.rohbeck@quantco.com>
2023-01-29 19:22:47 +0100

    Added tensor dimension associations

--- src/cellij/models.py
+++ src/cellij/models.py
@@ -1,12 +1,31 @@
+from typing import Dict, Optional, Union
+
+import pyro
+import torch
 from pyro.nn import PyroModule
 
 
 class Model(PyroModule):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
+    def __init__(self, device: Optional[Union[torch.device, int]] = None):
+        """Generative model base class.
 
-    def get_plates(self, *args, **kwargs):
+        The class uses a fixed number of tensor dimensions, associated with
+        - -1: samples
+        - -2: factors
+        - -3: features
+        - -4: views
+        - -5: groups
+
+        Parameters
+        ----------
+        device : Optional[Union[torch.device, int]], optional
+            Device to run the model on, by default None
+        """
+        super().__init__(name="Model")
+        self.device = device
+
+    def get_plates(self, *args, **kwargs) -> Dict[str, pyro.plate]:
         raise NotImplementedError()
 
-    def forward(self, *args, **kwargs):
+    def forward(self, *args, **kwargs) -> None:
         raise NotImplementedError()
