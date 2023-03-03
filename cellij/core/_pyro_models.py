0dddf3c0eb29560f342fe64d071da4f07397a2d6
martin.rohbeck <martin.rohbeck@quantco.com>
2023-03-03 15:21:37 +0100

    Added HS MOFA implementation

--- cellij/core/_pyro_models.py
+++ cellij/core/_pyro_models.py
@@ -3,63 +3,64 @@ import pyro.distributions as dist
 import torch
 from pyro.nn import PyroModule
 
-'''
-TODO: Every model class must inherit from Abstract Meta Model Class, e.g.
-
-class GenerativeModel(PyroModule):
-    def __init__(self, device: Optional[Union[torch.device, int]] = None):
-        """Generative model base class.
-
-        The class uses a fixed number of tensor dimensions, associated with
-        - -1: samples
-        - -2: factors
-        - -3: features
-        - -4: views
-        - -5: groups
-        Parameters
-        ----------
-        device : Optional[Union[torch.device, int]], optional
-            Device to run the model on, by default None
-        """
-        super().__init__(name="GenerativeModel")
-        self.device = device
-
-    def get_plates(self, *args, **kwargs) -> Dict[str, pyro.plate]:
-        raise NotImplementedError()
-    def forward(self, *args, **kwargs) -> None:
-        raise NotImplementedError()
-'''
-
 
 class MOFA_Model(PyroModule):
     def __init__(self, n_factors: int):
         super().__init__(name="MOFA_Model")
         self.n_factors = n_factors
+        self.n_features = None
+        self.n_feature_groups = None
+        self.n_obs = None
+        self.feature_offsets = None
+
+    def _setup(self, n_obs, feature_offsets):
+        # TODO: at some point replace n_obs with obs_offsets
+        self.n_obs = n_obs
+        self.n_features = feature_offsets[-1]
+        self.n_feature_groups = len(feature_offsets) - 1
+        self.feature_offsets = feature_offsets
 
     def forward(self, X):
         """Generative model for MOFA."""
         plates = self.get_plates()
 
-        with plates["obs"]:
-            z = pyro.sample("z", dist.Normal(torch.zeros(self.n_factors), torch.ones(self.n_factors)))
+        with plates["feature_groups"]:
+            feature_group_scale = pyro.sample("feature_group_scale", dist.HalfCauchy(torch.ones(1))).view(
+                -1, self.n_feature_groups
+            )
 
-        with plates["factors"]:
-            with plates["features"]:
-                w = pyro.sample("w", dist.Normal(torch.zeros(1), torch.ones(1)))
+        with plates["obs"], plates["factors"]:
+            z = pyro.sample("z", dist.Normal(torch.zeros(1), torch.ones(1))).view(-1, self.n_obs, self.n_factors, 1)
+
+        with plates["features"], plates["factors"]:
+            # implement the horseshoe prior
+            w_shape = (-1, 1, self.n_factors, self.n_features)
+            w_scale = pyro.sample("w_scale", dist.HalfCauchy(torch.ones(1))).view(w_shape)
+            w_scale = torch.cat(
+                [
+                    w_scale[..., self.feature_offsets[m] : self.feature_offsets[m + 1]]
+                    * feature_group_scale[..., m : m + 1]
+                    for m in range(self.n_feature_groups)
+                ],
+                dim=-1,
+            )
+            w = pyro.sample("w", dist.Normal(torch.zeros(1), w_scale)).view(w_shape)
 
         with plates["features"]:
-            sigma = pyro.sample("sigma", dist.InverseGamma(torch.tensor(3.0), torch.tensor(1.0)))
+            sigma = pyro.sample("sigma", dist.InverseGamma(torch.tensor(3.0), torch.tensor(1.0))).view(
+                -1, 1, 1, self.n_features
+            )
 
         with plates["obs"]:
-            pyro.sample(
-                "data",
-                dist.Normal(z @ w, torch.sqrt(sigma)),
-                obs=X.T,
-            )
+            prod = torch.einsum("...ikj,...ikj->...ij", z, w).view(-1, self.n_obs, 1, self.n_features)
+            y = pyro.sample("data", dist.Normal(prod, torch.sqrt(sigma)), obs=X.view(1, self.n_obs, 1, self.n_features))
+
+        return {"z": z, "w": w, "sigma": sigma, "y": y}
 
     def get_plates(self):
         return {
-            "obs": pyro.plate("obs", 136, dim=-2),
+            "obs": pyro.plate("obs", self.n_obs, dim=-3),
             "factors": pyro.plate("factors", self.n_factors, dim=-2),
-            "features": pyro.plate("features", 5000, dim=-1),
+            "features": pyro.plate("features", self.n_features, dim=-1),
+            "feature_groups": pyro.plate("feature_groups", self.n_feature_groups, dim=-1),
         }
