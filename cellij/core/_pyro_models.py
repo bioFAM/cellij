18cb13226573952ac4a20fcd120b633789bd4147
Martin Rohbeck <35061428+martinrohbeck@users.noreply.github.com>
2023-03-10 14:25:24 +0100

    Add first implementation to (sparse) MOFA (#28)
    Co-authored-by: Arber Qoku <arber.qoku@outlook.com>

--- cellij/core/_pyro_models.py
+++ cellij/core/_pyro_models.py
@@ -5,13 +5,10 @@ from pyro.nn import PyroModule
 
 
 class MOFA_Model(PyroModule):
-    def __init__(self, n_factors: int):
+    def __init__(self, n_factors: int, sparsity_prior: str):
         super().__init__(name="MOFA_Model")
         self.n_factors = n_factors
-        # self.n_features = None
-        # self.n_feature_groups = None
-        # self.n_obs = None
-        # self.feature_offsets = None
+        self.sparsity_prior = sparsity_prior
 
     def _setup(self, data):
         # TODO: at some point replace n_obs with obs_offsets
@@ -26,36 +23,78 @@ class MOFA_Model(PyroModule):
         """Generative model for MOFA."""
         plates = self.get_plates()
 
-        with plates["feature_groups"]:
-            feature_group_scale = pyro.sample("feature_group_scale", dist.HalfCauchy(torch.ones(1))).view(
-                -1, self.n_views
+        with plates["obs"], plates["factors"]:
+            z = pyro.sample("z", dist.Normal(torch.zeros(1), torch.ones(1))).view(
+                -1, self.n_obs, self.n_factors, 1
             )
 
-        with plates["obs"], plates["factors"]:
-            z = pyro.sample("z", dist.Normal(torch.zeros(1), torch.ones(1))).view(-1, self.n_obs, self.n_factors, 1)
+        if self.sparsity_prior == "Horseshoe":
+            with plates["feature_groups"]:
+                feature_group_scale = pyro.sample(
+                    "feature_group_scale", dist.HalfCauchy(torch.ones(1))
+                ).view(-1, self.n_views)
 
         with plates["features"], plates["factors"]:
-            # implement the horseshoe prior
-            w_shape = (-1, 1, self.n_factors, self.n_features)
-            w_scale = pyro.sample("w_scale", dist.HalfCauchy(torch.ones(1))).view(w_shape)
-            w_scale = torch.cat(
-                [
-                    w_scale[..., self.feature_idx[view]]
-                    * feature_group_scale[..., idx : idx + 1]
-                    for idx, view in enumerate(self.views)
-                ],
-                dim=-1,
+            if self.sparsity_prior == "Spikeandslab-Beta":
+                a_beta = torch.tensor(1e-3)
+                b_beta = torch.tensor(1e-3)
+                # a_gamma = pyro.param(torch.tensor(1e-3))
+                # b_gamma = pyro.param(torch.tensor(1e-3))
+                # tau = pyro.sample("tau", dist.Gamma(a_gamma, b_gamma)).view(-1, 1, self.n_factors, self.n_features)
+                w_scale = pyro.sample("w_scale", dist.Beta(a_beta, b_beta)).view(
+                    -1, 1, self.n_factors, self.n_features
+                )
+                # slab = pyro.sample("slab", dist.Normal(torch.zeros(1), torch.tensor(1.0))).view(
+                #     -1, 1, self.n_factors, self.n_features
+                # )
+                # w = pi * slab
+            elif self.sparsity_prior == "Spikeandslab-ContinuousBernoulli":
+                raise NotImplementedError()
+            elif self.sparsity_prior == "Spikeandslab-RelaxedBernoulli":
+                raise NotImplementedError()
+            elif self.sparsity_prior == "Spikeandslab-Enumeration":
+                raise NotImplementedError()
+            elif self.sparsity_prior == "Spikeandslab-Lasso":
+                raise NotImplementedError()
+            elif self.sparsity_prior == "Horseshoe":
+                # implement the horseshoe prior
+                w_shape = (-1, 1, self.n_factors, self.n_features)
+                w_scale = pyro.sample("w_scale", dist.HalfCauchy(torch.ones(1))).view(
+                    w_shape
+                )
+                w_scale = torch.cat(
+                    [
+                        w_scale[..., self.feature_idx[view]]
+                        * feature_group_scale[..., idx : idx + 1]
+                        for idx, view in enumerate(self.views)
+                    ],
+                    dim=-1,
+                )
+            elif self.sparsity_prior == "Lasso":
+                raise NotImplementedError()
+            elif self.sparsity_prior == "Nonnegative":
+                raise NotImplementedError()
+            else:
+                w_scale = torch.tensor(1.0)
+
+            w = pyro.sample("w", dist.Normal(torch.zeros(1), w_scale)).view(
+                -1, 1, self.n_factors, self.n_features
             )
-            w = pyro.sample("w", dist.Normal(torch.zeros(1), w_scale)).view(w_shape)
 
         with plates["features"]:
-            sigma = pyro.sample("sigma", dist.InverseGamma(torch.tensor(3.0), torch.tensor(1.0))).view(
-                -1, 1, 1, self.n_features
-            )
+            sigma = pyro.sample(
+                "sigma", dist.InverseGamma(torch.tensor(3.0), torch.tensor(1.0))
+            ).view(-1, 1, 1, self.n_features)
 
         with plates["obs"]:
-            prod = torch.einsum("...ikj,...ikj->...ij", z, w).view(-1, self.n_obs, 1, self.n_features)
-            y = pyro.sample("data", dist.Normal(prod, torch.sqrt(sigma)), obs=X.view(-1, self.n_obs, 1, self.n_features))
+            prod = torch.einsum("...ikj,...ikj->...ij", z, w).view(
+                -1, self.n_obs, 1, self.n_features
+            )
+            y = pyro.sample(
+                "data",
+                dist.Normal(prod, torch.sqrt(sigma)),
+                obs=X.view(-1, self.n_obs, 1, self.n_features),
+            )
 
         return {"z": z, "w": w, "sigma": sigma, "y": y}
 
--- cellij/core/models.py
+++ cellij/core/models.py
@@ -13,26 +13,19 @@ from cellij.core._pyro_models import MOFA_Model
 class MOFA(FactorModel):
     """Model for Multi-Omics Factor Analysis."""
 
-    def __init__(self, n_factors, **kwargs):
+    def __init__(self, n_factors, sparsity_prior="spikeandslab", **kwargs):
         # If default variable is provided in kwargs, overwrite it
         mofa_defaults = {
-            "model": MOFA_Model(n_factors=n_factors),
+            "model": MOFA_Model(n_factors=n_factors, sparsity_prior=sparsity_prior),
             "guide": "AutoNormal",
             "trainer": "Adam",
         }
-        # We do not type check kwargs, because we rely on type checking
-        # in the FactorModel class
-        if "guide" in kwargs:
-            mofa_defaults["guide"] = kwargs["guide"]
-            kwargs.pop("guide")
-        if "trainer" in kwargs:
-            mofa_defaults["trainer"] = kwargs["trainer"]
-            kwargs.pop("trainer")
+
+        kwargs = {**mofa_defaults, **kwargs}
 
         super(MOFA, self).__init__(
             n_factors=n_factors,
-            **mofa_defaults,
-            **kwargs,
+            **kwargs
         )
 
     def add_data(
