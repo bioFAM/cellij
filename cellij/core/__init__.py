61125ad99eee7467816674302d7ef8907693ffb4
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-04-01 18:11:45 +0200

    removed subfolder __init__s

--- cellij/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._data import DataContainer, Importer
-from ._factormodel import FactorModel
-from ._group import Group
-from ._pyro_models import MOFA_Model
-from .models import MOFA
-from .synthetic import DataGenerator
