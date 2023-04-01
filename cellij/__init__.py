0eabb0809d6bc851f8d7b9104baeae6930b49bb2
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-04-01 17:46:21 +0200

    fixed __init__s

--- cellij/__init__.py
+++ cellij/__init__.py
@@ -1,3 +1,3 @@
 from .core import _data, _factormodel, _group, _pyro_models, models, synthetic
-from .tools import _get_w, _get_z, _get_from_param_storage
+from .tools import inspect
 from .utils import _get_param_storage_key_prefix
--- cellij/tools/__init__.py
+++
@@ -1,1 +0,0 @@
-from .inspect import _get_w, _get_z, _get_from_param_storage
