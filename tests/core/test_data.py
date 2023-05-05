1cd45bf39dd337bbcbfa83d248da0f9b45a06bd1
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-05-05 16:07:47 +0200

    fixed rng

--- tests/core/test_data.py
+++ tests/core/test_data.py
@@ -14,6 +14,7 @@ np.random.seed(8)
 class core_TestClass(unittest.TestCase):
     def setUp(self):
         self.model = cellij.core.models.MOFA(n_factors=2)
+        self.rng = np.random.default_rng(8)
 
     def test_na_strategy_mean(self):
         data = anndata.AnnData(
@@ -52,31 +53,31 @@ class core_TestClass(unittest.TestCase):
         assert all(self.model.data.values[:, 1] == [1, 2, 6])
 
     def test_na_strategy_knn(self):
-        data = np.round(np.random.randn(100).reshape(10, 10))
-        data[np.where(data > 2)] = np.nan
+        data = np.round(self.rng.random(size=(10,10)), 1)
+        data[np.where(data == 1)] = np.nan
         data = anndata.AnnData(
             pd.DataFrame(data=data, columns=[f"feat{i}" for i in range(10)], index=[f"obs{i}" for i in range(10)])
         )
         self.model.add_data(name="toydata", data=data, na_strategy="knn")
 
-        np.testing.assert_almost_equal(np.round(self.model.data.values[9, 4], 4), 0.0101)
+        np.testing.assert_almost_equal(np.round(self.model.data.values[0, 1], 4), 0.4242)
 
     def test_na_strategy_knn_by_features(self):
-        data = np.round(np.random.randn(100).reshape(10, 10))
-        data[np.where(data > 2)] = np.nan
+        data = np.round(self.rng.random(size=(10,10)), 1)
+        data[np.where(data == 1)] = np.nan
         data = anndata.AnnData(
             pd.DataFrame(data=data, columns=[f"feat{i}" for i in range(10)], index=[f"obs{i}" for i in range(10)])
         )
         self.model.add_data(name="toydata", data=data, na_strategy="knn_by_features")
 
-        np.testing.assert_almost_equal(np.round(self.model.data.values[9, 4], 4), -1.3333)
+        np.testing.assert_almost_equal(np.round(self.model.data.values[0, 1], 4), 0.2333)
 
     def test_na_strategy_knn_by_observations(self):
-        data = np.round(np.random.randn(100).reshape(10, 10))
-        data[np.where(data > 2)] = np.nan
+        data = np.round(self.rng.random(size=(10,10)), 1)
+        data[np.where(data == 1)] = np.nan
         data = anndata.AnnData(
             pd.DataFrame(data=data, columns=[f"feat{i}" for i in range(10)], index=[f"obs{i}" for i in range(10)])
         )
         self.model.add_data(name="toydata", data=data, na_strategy="knn_by_observations")
 
-        np.testing.assert_almost_equal(np.round(self.model.data.values[9, 4], 4), -0.6667)
+        np.testing.assert_almost_equal(np.round(self.model.data.values[0, 1], 4), 0.3667)
