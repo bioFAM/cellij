22f28bda6c71bb533442704f6a3f2497fe4f95cc
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-05-05 18:26:45 +0200

    Added other imputation strategies, logger and fixed codecov

--- cellij/__init__.py
+++ cellij/__init__.py
@@ -1,3 +1,3 @@
 from .core import _data, _factormodel, _group, _pyro_models, models, synthetic, sparsity_priors
 from .tools import evaluation
-from .utils import load_model
+from .utils import load_model, impute_data
