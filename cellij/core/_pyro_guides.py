935b75d8fe30267968969dd57889297e9bc9a48a
Martin Rohbeck <35061428+martinrohbeck@users.noreply.github.com>
2023-08-03 20:53:51 +0200

    Fix critical bug in Var Distr

--- cellij/core/_pyro_guides.py
+++ cellij/core/_pyro_guides.py
@@ -135,7 +135,7 @@ class QDist(PyroModule):
         return (loc + scale.pow(2) / 2).exp()
 
     def _median_log_normal(self, site_name: str) -> torch.Tensor:
-        loc, scale = self._get_loc_and_scale(site_name)
+        loc, _ = self._get_loc_and_scale(site_name)
         return loc.exp()
 
     def _mode_log_normal(self, site_name: str) -> torch.Tensor:
